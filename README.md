76e2a6646e60ef1e1f290ffa2496610118554d0d
devquercus <devquercus@gmail.com>
2022-02-14 09:42:02 +0200

    Fix cinnectivity issues

--- README.md
+++ README.md
@@ -6,13 +6,3 @@ You need to set the path for the id_rsa file and the keypass for that file.
 
 If you want to upload any files in the server you must define the local and remote paths.
 
-TODO:
-- Make a function to upload the files and check if the defined remote path exists or not and create the appropriate path
- if needed
-- Make a log of the connections and any changes made to the server 
-- Add the POST request to the server to notify that the data has been uploaded
-
-
-Credits: 
--   Dimitris Kasabalis
-    email: dimitriskasabalis@gmail.com
--- sftp_client.py
+++ sftp_client.py
@@ -2,7 +2,7 @@ import os
 import paramiko
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 
 '''
 This script uses the paramiko module to make a connection to an sftp server. 
@@ -11,36 +11,26 @@ You need to set the path for the id_rsa file and the keypass for that file.
 
 If you want to upload any files in the server you must define the local and remote paths.
 
-TODO:
-- Make a function to upload the files and check if the defined remote path exists or not and create the appropriate path 
-if needed
-- Make a log of the connections and any changes made to the server 
-- Add the POST request to the server to notify that the data has been uploaded
-
-Credits: 
--   Dimitris Kasabalis
-    email: dkasampa@agro.auth.gr
 '''
 
+
 def create_client(host, port, username, keypath, keypass):
-    '''
+    """
 
-    :param host: The server address
-    :param port: The port
-    :param username: Specify the name of the user
-    :param keypath: Specify the location of the id_rsa file in your machine
-    :param keypass: Specify the password for the id_rsa key
+    host: The server address
+    port: The port
+    username: Specify the name of the user
+    keypath: Specify the location of the id_rsa file in your machine
+    keypass: Specify the password for the id_rsa key
 
-    '''
-    global transport
+    """
     transport = paramiko.Transport((host, port))
     mykey = paramiko.RSAKey.from_private_key_file(keypath, keypass)
     print("Connecting...")
     transport.connect(username=username, pkey=mykey)
-    global sftp
     sftp = paramiko.SFTPClient.from_transport(transport)
     print("Connected.")
-    print(sftp.listdir())
+    return sftp
 
 
 host = os.getenv("host")
@@ -49,7 +39,7 @@ username = os.getenv("username")
 keypath = os.getenv("keypath")
 keypass = os.getenv("keypass")
 
-create_client(host=host, port=port, username=username, keypath=keypath, keypass=keypass)
+sftpclient = create_client(host=host, port=port, username=username, keypath=keypath, keypass=keypass)
 
 # Uncomment of you want to upload files
 # localPath = ""
@@ -57,6 +47,10 @@ create_client(host=host, port=port, username=username, keypath=keypath, keypass=
 
 # sftp.put(localpath=localPath, remotepath=remotePath)
 
-sftp.close()
-transport.close()
-print("Closed connection.")
+dirlist = sftpclient.listdir('.')
+for row in dirlist:
+    print(row)
+
+sftpclient.close()
+
+productsList = ["S02P02", "S06P01", "S06P04"]
